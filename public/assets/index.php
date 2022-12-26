2d0b860e399aff7bfe481b58787128eb35309755
Aralov Mikhail <aralov.mykhailo@gmail.com>
2022-12-26 17:16:35 +0200

    fix base layout

--- public/assets/index.php
+++ public/assets/index.php
@@ -1,7 +1,18 @@
 <?php get_header(); ?>
 
     <main>
-        <?php the_content(); ?>
+        <div class="container-lg pt-3 pt-xl-4 mt-2 mt-lg-3 mb-3 mb-lg-5 pb-3 pb-lg-5">
+            <div class="mb-3">
+            <?php
+                if ( function_exists('yoast_breadcrumb') ) {
+                    yoast_breadcrumb( '<div class="breadcrumbs" id="breadcrumbs">','</div>' );
+                }
+            ?>
+            </div>
+           <div class="content">
+                <?php the_content(); ?>
+           </div
+        </div>
     </main>
 
 <?php get_footer(); ?>
